c664702616dd9c6dc62a22b3f99cf7d110d3d01e
Ruslan Arshynnyk <ruslan.arshynnyk@litslink.com>
2020-01-18 00:30:53 +0200

    Added CORS support;

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -8,8 +8,14 @@ router.get('/', (req, res, next) => {
 })
 
 router.post('/', (req, res, next) => {
-  res.status(200).json({
-    message: 'orders POST'
+  const order = {
+    productId: req.body.productId,
+    quantity: req.body.quantity
+  }
+
+  res.status(201).json({
+    message: 'Order was successfully created',
+    data: order,
   })
 })
 
