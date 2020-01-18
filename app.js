3f495884bde34c21017e831c033ccdc69042ca89
Ruslan Arshynnyk <ruslan.arshynnyk@litslink.com>
2020-01-19 01:15:57 +0200

    Added global errors handler; Added Cors package; Finished Basic project setup;

--- app.js
+++ app.js
@@ -1,55 +1,31 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const morgan = require("morgan");
-const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const authRoutes = require("./api/routes/auth");
-const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
+const productRoutes = require("./api/routes/products");
 
-mongoose.connect(
-  "mongodb+srv://" +
-    process.env.MONGO_USERNAME +
-    ":" +
-    process.env.MONGO_PASSWORD +
-    "@cluster0-jcmsn.mongodb.net/test?retryWrites=true&w=majority",
-  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
-);
+const errorHandler = require("./helpers/errorsHandler");
 
+mongoose.connect(process.env.MONGO_CONNECT, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+});
+
+app.use(cors());
 app.use(morgan("dev"));
-
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "*");
-
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
-    return res.status(200).json({});
-  }
-  next();
-});
-
-app.use("/products", productRoutes);
-app.use("/orders", orderRoutes);
 app.use("/auth", authRoutes);
+app.use("/orders", orderRoutes);
+app.use("/products", productRoutes);
 
-app.use((req, res, next) => {
-  const error = new Error("Not found");
-  error.status = 404;
-  next(error);
-});
-
-app.use((error, req, res, next) => {
-  res.status(error.status || 500);
-  res.json({
-    error: {
-      message: error.message
-    }
-  });
-});
+app.use(errorHandler);
 
 module.exports = app;
